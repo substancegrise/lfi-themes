46a500e359056f8c55c24a93e53434e2527b4541
domdom <dominique@ifive.io>
2017-02-01 16:00:24 +0100

    LFI Theme : Added presentation page and video players + overlay icons and share button + responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,6 +17,92 @@ $(document).ready(function() {
 	    }
 	});
 	
+	if($("body.formation-single .share-icon").length){		  
+		new ShareButton({ui: {flyout:"bottom right",buttonText:""}, networks: {reddit: {enabled:false},linkedin: {enabled:false},whatsapp: {enabled:false},pinterest: {enabled:false},email: {enabled:false}}});
+	}
+	if($("body.formations .share-icon").length){
+		new ShareButton({
+			ui : {
+				flyout : "middle left",
+				buttonText : ""
+			},
+			networks : {
+				facebook:{
+					before: function(element) {
+						this.url = element.getAttribute("data-url");
+					}
+				},
+				twitter:{
+					before: function(element) {
+						this.url = element.getAttribute("data-url");
+					}
+				},
+				googlePlus:{
+					before: function(element) {
+						this.url = element.getAttribute("data-url");
+					}
+				},
+				reddit : {
+					enabled : false
+				},
+				linkedin : {
+					enabled : false
+				},
+				whatsapp : {
+					enabled : false
+				},
+				pinterest : {
+					enabled : false
+				},
+				email : {
+					enabled : false
+				}
+			}
+		});
+	}
+
+	if($(".list-item .share-icon").length || $(".full-actus .share-icon").length){
+		new ShareButton({
+			ui : {
+				flyout : "middle right",
+				buttonText : ""
+			},
+			networks : {
+				facebook:{
+					before: function(element) {
+						this.url = element.getAttribute("data-url");
+					}
+				},
+				twitter:{
+					before: function(element) {
+						this.url = element.getAttribute("data-url");
+					}
+				},
+				googlePlus:{
+					before: function(element) {
+						this.url = element.getAttribute("data-url");
+					}
+				},
+				reddit : {
+					enabled : false
+				},
+				linkedin : {
+					enabled : false
+				},
+				whatsapp : {
+					enabled : false
+				},
+				pinterest : {
+					enabled : false
+				},
+				email : {
+					enabled : false
+				}
+			}
+		}); 
+	}
+	
+	
 	$('.nav-button').click(function(e){
 		
 		$('.nav-button.active').removeClass('active');
@@ -98,6 +184,102 @@ $(document).ready(function() {
 			$.request('onPaginate', {success:function(data){addActusSlide(data);}, data: {page: thispage.page}});
 			currentPage = thispage.page/1;
 		}
+	});
+	
+	
+	/************* VIDEO PLAYER ***************/
+	var vie = (function(){
+		
+		var ua = window.navigator.userAgent;
+		
+		var msie = ua.indexOf('MSIE ');
+		if (msie > 0) {
+		    // IE 10 or older => return version number
+		    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+		}
+		
+		var trident = ua.indexOf('Trident/');
+		if (trident > 0) {
+		    // IE 11 => return version number
+		    var rv = ua.indexOf('rv:');
+		    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+		}
+		
+		var edge = ua.indexOf('Edge/');
+		if (edge > 0) {
+		   // IE 12 => return version number
+		   return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+		    }
+	
+	}());
+	
+	var exitFullScreenHandler = function(){
+		if(!$("body").hasClass("playerfullscreen"))
+			$("body").addClass("playerfullscreen")
+		else{
+			$("body").removeClass("playerfullscreen");
+			$("#bgvid")[0].pause();
+			$("#bgvid").fadeOut("slow");
+		}
+	};
+	var exitFullScreenMobileHandler = function(){
+		if($("#bgvid").length > 0){
+			if(!$("body").hasClass("playerfullscreen")){
+				$("body").addClass("playerfullscreen")
+			} else {
+				$("body").removeClass("playerfullscreen");
+				$("#bgvid")[0].pause();
+				$("#bgvid").fadeOut("slow");
+			}
+		}
+	};
+		
+	var playVideoFullScreenMobile = function(video) {
+		if (document.addEventListener)
+		{
+		    document.addEventListener('webkitfullscreenchange', exitFullScreenMobileHandler, false);
+		    document.addEventListener('mozfullscreenchange', exitFullScreenMobileHandler, false);
+		    document.addEventListener('fullscreenchange', exitFullScreenMobileHandler, false);
+		    document.addEventListener('MSFullscreenChange', exitFullScreenMobileHandler, false);
+		    
+		    video.addEventListener('webkitendfullscreen', function(){
+		    		$("#bgvid")[0].pause();
+					$("#bgvid").fadeOut("slow");
+		    	}, false);
+		}
+				
+		var el = video, rfs = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen || el.webkitEnterFullScreen ;
+		if(typeof rfs!="undefined" && rfs){
+		  rfs.call(el);
+		} else {
+			alert("La video ne peux pas être vue sur ce navigateur");
+		}
+	};
+	
+	 var playVideoFullScreen = function(video) {
+		
+		if (document.addEventListener){
+		    document.addEventListener('webkitfullscreenchange', exitFullScreenHandler, false);
+		    document.addEventListener('mozfullscreenchange', exitFullScreenHandler, false);
+		    document.addEventListener('fullscreenchange', exitFullScreenHandler, false);
+		    document.addEventListener('MSFullscreenChange', exitFullScreenHandler, false);
+		}
+	
+		var el = video, rfs = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullscreen || el.webkitEnterFullScreen ;
+		if(typeof rfs!="undefined" && rfs){
+		  rfs.call(el);
+		} else {
+			alert("La video ne peux pas être vue sur ce navigateur");
+		}
+	
+		video.play();
+	};
+
+	var vid = document.getElementById("bgvid");
+	
+	$(".teaser").click(function(){
+		playVideoFullScreen($(vid)[0]);							
+		return false;
 	});
 });
 
